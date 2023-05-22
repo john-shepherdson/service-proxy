9e0f91e55e8ec0724c515ebaa02571b954a03648
Konstantinos Georgilakis <cgeorgilakis@grnet.gr>
2023-05-22 12:44:15 +0300

    Add authnAuthority and voPersonID user attribute to event details

--- server-spi-private/src/main/java/org/keycloak/events/EventBuilder.java
+++ server-spi-private/src/main/java/org/keycloak/events/EventBuilder.java
@@ -43,7 +43,9 @@ import java.util.stream.Stream;
 public class EventBuilder {
 
     private static final Logger log = Logger.getLogger(EventBuilder.class);
-
+    public static final String AUTHN_AUTHORITY ="authnAuthority";
+    public static final String VO_PERSON_ID ="voPersonID";
+    
     private final KeycloakSession session;
     private EventStoreProvider store;
     private List<EventListenerProvider> listeners;
@@ -124,6 +126,10 @@ public class EventBuilder {
 
     public EventBuilder user(UserModel user) {
         event.setUserId(user == null ? null : user.getId());
+        if (user.getAttributeStream(AUTHN_AUTHORITY).count() >0)
+            detail(AUTHN_AUTHORITY,user.getAttributeStream(AUTHN_AUTHORITY).collect(Collectors.toList()));
+        if (user.getAttributeStream(VO_PERSON_ID).count() >0)
+            detail(VO_PERSON_ID,user.getAttributeStream(VO_PERSON_ID).collect(Collectors.toList()));
         return this;
     }
 
