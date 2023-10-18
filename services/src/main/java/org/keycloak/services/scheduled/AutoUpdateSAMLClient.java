511e62d2e69b48b1c4150f42c2e55bb6810a39b7
cgeorgilakis-grnet <cgeorgilakis@admin.grnet.gr>
2023-10-18 10:11:53 +0300

    RCIAM-1205 Bug corrections related to SAML Autoupdated client and add debug log to SAML Federation mapper

--- services/src/main/java/org/keycloak/services/scheduled/AutoUpdateSAMLClient.java
+++ services/src/main/java/org/keycloak/services/scheduled/AutoUpdateSAMLClient.java
@@ -66,7 +66,7 @@ public class AutoUpdateSAMLClient implements ScheduledTask {
             });
 
             inputStream.close();
-        } catch (IOException e) {
+        } catch (IOException | ErrorResponseException e) {
             e.printStackTrace();
         }
     }
